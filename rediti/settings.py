e8fc2c5cbbfd66d87816a36a70958165249e34ad
AMANDA <amandasavluchinske@gmail.com>
2018-12-29 22:35:05 -0300

    added user model

--- rediti/settings.py
+++ rediti/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+AUTH_USER_MODEL = 'users.User'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'common',
+    'users',
 ]
 
 MIDDLEWARE = [
---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from common.models import IndexedTimeStampedModel
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin, IndexedTimeStampedModel):
+    name = models.CharField(max_length=60, blank=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=20)
+    karma = models.IntegerField(blank=True, null=True)
+    avatar = models.ImageField(blank=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.username
